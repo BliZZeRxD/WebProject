6e2ea80b50ee92e7c24edba3db031a4e115f8a5d
Ayan <40315034+BliZZeRxD@users.noreply.github.com>
2020-04-26 23:14:01 +0300

    Add files via upload

---
+++ backend/api/views/__init__.py
@@ -0,0 +1,6 @@
+from .generic import BookLists, BookListDetail
+from .FBV import book_list_book, feedback_list
+from .CBV import ShopLists, ContactList 
+
+from .auth import UserList, login, logout
+from .base import index
